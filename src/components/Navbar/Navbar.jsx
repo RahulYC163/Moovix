d8f70f2172216afb284868d3591294a69848196c
Rahul Y C <rahulyc994@gmail.com>
2025-04-30 20:35:56 +0530

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Navbar.css';
+import DarkMode from '../DarkMode/DarkMode';
+import Fire from '../../assets/fire.png';
+import Star from '../../assets/glowing-star.png';
+import Party from '../../assets/partying-face.png';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Moovix</h1>
+
+      <div className="navbar_links">
+        <DarkMode />
+        <NavLink to="/">
+          Popular <img src={Fire} alt="Fire emoji" className="navbar_emoji" />
+        </NavLink>
+
+        <NavLink to="/top_rated">
+          Top Rated <img src={Star} alt="Star emoji" className="navbar_emoji" />
+        </NavLink>
+
+        <NavLink to="/upcoming">
+          Upcoming
+          <img src={Party} alt="Party emoji" className="navbar_emoji" />
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
